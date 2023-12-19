c53aeacce0a316dc4f1e81c42a9a62905846da24
maxthemus <max.somerville62@gmail.com>
2023-12-20 12:20:57 +1300

    audio input recording

--- outputStream.py
+++ outputStream.py
@@ -1,41 +1,40 @@
 import sounddevice as sd
 import numpy as np
-import pygame
 import sys
-import tty
-import termios
+import asyncio
 
+#print(sd.query_devices())
+sample_rate = 44100
+channels = 2
+duration = 1  # in seconds
+index = 0
 
-def audio_stream(callback, channels=2, samplerate=44100):
-    stream = sd.InputStream(callback=callback, channels=channels, samplerate=samplerate)
-    with stream:
-        print("Streaming audio. Press Ctrl+C to stop.")
-        running = True
-        while running:
-            sd.sleep(100)
+audio_data = np.zeros((0, channels), dtype=np.float32)
 
-            x = 0
-            while x != chr(27): # ESC
-                x=sys.stdin.read(1)[0]
-                running = False
-                print("You pressed", x)
-            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)              
+async def audio_stream(callback):
+    print(sd.query_devices())
+    stream = sd.InputStream(callback=callback, samplerate=sample_rate)
+    stream.start()
+
+    sd.sleep(5000)
+
+    stream.stop()
+    print(audio_data)
+    print(np.size(audio_data))
 
 # Callback function to process audio data (you can customize this function)
 def audio_callback(indata, frames, time, status):
-    print(indata)
+    global audio_data
     if status:
         print(status)
-    # Process the audio data as needed
-    # For example, you can save it, analyze it, or perform real-time processing
-
-#INIT KEybaord
-orig_settings = termios.tcgetattr(sys.stdin)
-tty.setcbreak(sys.stdin)
+    audio_data = np.vstack((audio_data, indata))
 
 
 # Start the audio stream with the callback function
-audio_stream(audio_callback)
+asyncio.run(audio_stream(audio_callback))
 
-print("DONE")
+sd.play(audio_data, sample_rate)
+sd.wait()
+
+
 sys.exit()
