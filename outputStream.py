2a7402025912491c598d7e580ca7a28b462e28f6
maxthemus <max.somerville62@gmail.com>
2023-12-22 09:01:41 +1300

    updates

--- outputStream.py
+++ outputStream.py
@@ -1,32 +1,38 @@
 import sounddevice as sd
 import numpy as np
 import sys
+import asyncio
 
 #print(sd.query_devices())
 sample_rate = 44100
 channels = 2
 duration = 5  # in seconds
 
-def audio_stream(callback, channels=2, samplerate=44100):
-    stream = sd.InputStream(callback=callback, channels=channels, samplerate=samplerate)
-    with stream:
-        print("Streaming audio. Press Ctrl+C to stop.")
-        running = True
-        while running:
-            sd.sleep(100)
-                   
+insertIndex = 0
+audio_data = []
+
+async def audio_stream(callback):
+    sd.default.device = "default"
+    print(sd.query_devices())
+    stream = sd.InputStream(callback=callback, samplerate=sample_rate, channels=2)
+    stream.start()
+
+    sd.sleep(duration * 1000)
+
+
+    stream.stop()
+
 
 # Callback function to process audio data (you can customize this function)
 def audio_callback(indata, frames, time, status):
     global audio_data
     if status:
         print(status)
-    # Process the audio data as needed
-    # For example, you can save it, analyze it, or perform real-time processing
+    for sub_array in indata:
+        audio_data.append(sub_array.tolist())
+    #np.append(audio_data, indata, axis=0)
+    #audio_data = np.vstack((audio_data, indata))
 
-
-
-print(sd.query_devices())
 
 # Start the audio stream with the callback function
 asyncio.run(audio_stream(audio_callback))
